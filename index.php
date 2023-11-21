ff94bd5f6008a79c076a0dd3fe4d83534e712fc8
Mark Lape <121615238+Poowd@users.noreply.github.com>
2023-11-21 13:50:26 +0800

    ...

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Enrollment System</title>
 </head>
 <body>
     <h1>sino kaya unang mag p-pull dito sa enrollment system na to.</h1>
